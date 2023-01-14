559f0312c2e1166803bfebe49eacaef4697a99aa
galaxxy-wanderer <113029331+galaxxy-wanderer@users.noreply.github.com>
2023-01-14 02:34:50 +0100

    Fahrenheit conversion upd, icon upd, style upd

--- src/script.js
+++ src/script.js
@@ -18,69 +18,80 @@ currentDate.innerHTML = `${day} ${hour}:${minute}`;
 function showCity(event) {
   event.preventDefault();
   let cityInput = document.querySelector("#city-input");
-  let searchedCity = document.querySelector("#searched-city");
-  searchedCity.innerHTML =
-    cityInput.value[0].toUpperCase() + cityInput.value.substring(1);
-  let apiKey = "c8735bb7e8e2f8d8a38c7501f3cd47d3";
+  let apiKey = "535cacbb3f8a0df0aeb4790235b9541f";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&appid=${apiKey}&&units=metric`;
-  //console.log(apiUrl);
+
+  console.log(apiUrl);
   axios.get(apiUrl).then(displayWeather);
 }
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", showCity);
 
-function showCity2(event) {
-  event.preventDefault();
-  let cityInput = document.querySelector("#city-input");
-  let searchedCity = document.querySelector("#searched-city");
-  searchedCity.innerHTML =
-    cityInput.value[0].toUpperCase() + cityInput.value.substring(1);
-  let apiKey = "c8735bb7e8e2f8d8a38c7501f3cd47d3";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&appid=${apiKey}&&units=metric`;
-  //console.log(apiUrl);
-  axios.get(apiUrl).then(displayWeather);
-}
 let searchButton = document.querySelector("#search-button");
-searchButton.addEventListener("click", showCity2);
+searchButton.addEventListener("click", showCity);
 
 function displayWeather(response) {
-  let temp = Math.round(response.data.main.temp);
+  tempInC.classList.add("active");
+  let searchedCity = document.querySelector("#searched-city");
+  searchedCity.innerHTML = response.data.name;
+  celsiusTemp = response.data.main.temp;
+  let temp = Math.round(celsiusTemp);
   let tempInCity = document.querySelector("#current-temp");
   tempInCity.innerHTML = temp;
-  let max = document.querySelector("#temp-max");
-  max.innerHTML = Math.round(response.data.main.temp_max);
-  let min = document.querySelector("#temp-min");
-  min.innerHTML = Math.round(response.data.main.temp_min);
+  let rain = document.querySelector("#precipitation");
+  rain.innerHTML = response.data.weather[0].description;
+  let wind = document.querySelector("#wind-speed");
+  wind.innerHTML = Math.round(response.data.wind.speed);
   let humidity = document.querySelector("#temp-hum");
   humidity.innerHTML = Math.round(response.data.main.humidity);
   let feelsLike = document.querySelector("#temp-feels");
   feelsLike.innerHTML = Math.round(response.data.main.feels_like);
-}
-function displayWeather2(response) {
-  let temp = Math.round(response.data.main.temp);
-  let tempInCity = document.querySelector("#current-temp");
-  tempInCity.innerHTML = temp;
-  let currentCity = document.querySelector("#searched-city");
-  currentCity.innerHTML = response.data.name;
-  let max = document.querySelector("#temp-max");
-  max.innerHTML = Math.round(response.data.main.temp_max);
-  let min = document.querySelector("#temp-min");
-  min.innerHTML = Math.round(response.data.main.temp_min);
-  let humidity = document.querySelector("#temp-hum");
-  humidity.innerHTML = Math.round(response.data.main.humidity);
-  let feelsLike = document.querySelector("#temp-feels");
-  feelsLike.innerHTML = Math.round(response.data.main.feels_like);
+  let icon = document.querySelector("#icon");
+  icon.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
 }
 
 function getLocation() {
   function retrievePosition(position) {
-    let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+    tempInC.classList.add("active");
+    let apiKey = "535cacbb3f8a0df0aeb4790235b9541f";
     let lat = position.coords.latitude;
     let lon = position.coords.longitude;
     let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
-    axios.get(url).then(displayWeather2);
+    axios.get(url).then(displayWeather);
   }
   navigator.geolocation.getCurrentPosition(retrievePosition);
 }
 let locationButton = document.querySelector("#location-button");
 locationButton.addEventListener("click", getLocation);
+
+function cToF(event) {
+  event.preventDefault();
+  tempInF.classList.add("active");
+  tempInC.classList.remove("active");
+  let fTemp = (celsiusTemp * 9) / 5 + 32;
+  let fCurrentTemp = document.querySelector("#current-temp");
+  fCurrentTemp.innerHTML = Math.round(fTemp);
+}
+let tempInF = document.querySelector("#temp-F");
+tempInF.addEventListener("click", cToF);
+
+function fToC(event) {
+  event.preventDefault();
+  tempInC.classList.add("active");
+  tempInF.classList.remove("active");
+  let cCurrentTemp = document.querySelector("#current-temp");
+  cCurrentTemp.innerHTML = Math.round(celsiusTemp);
+}
+let tempInC = document.querySelector("#temp-C");
+tempInC.addEventListener("click", fToC);
+
+let celsiusTemp = null;
+
+window.onload = function searchBarcelona() {
+  tempInC.classList.add("active");
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Barcelona&appid=535cacbb3f8a0df0aeb4790235b9541f&&units=metric`;
+  axios.get(apiUrl).then(displayWeather);
+};
